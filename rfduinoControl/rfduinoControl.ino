8e69c1c6576b34af2ed6cf05b6cfe2c931150d31
Jef Pearlman <github@jef-pearlman.net>
2015-05-02 15:26:08 -0700

    Motor and igniter state tracking/differencing; lost message detection; fault reporting.
    1) Only send updates to motor or igniter if state has
    changed. Resolves #17.
    
    2) Use message counter to detect and report lost messages. Note that
    if it turns out that BLE will not lose messages without disconnecting,
    this code and the transmitted counter may be superfluous.
    
    3) Report actual fault bits when a fault occurs. Resolves #5.

--- rfduinoControl/rfduinoControl.ino
+++ rfduinoControl/rfduinoControl.ino
@@ -9,7 +9,10 @@
 #define FAULT 0x01
 #define PROTOCOL_VERSION 0
 
-byte motors[3] = {MOTOR1,MOTOR2,MOTOR3};
+byte motorIndexes[3] = {MOTOR1,MOTOR2,MOTOR3};
+byte motorStates[3][2] = {{0, 0}, {0, 0}, {0, 0}};
+byte igniterState = 0;
+byte expectedMsgCounter = 0xff;
 int Vset;
 int motorTimeout = 1000;
 int igniterTimeout = 1000;
@@ -27,6 +30,7 @@ uint8_t wireAck;
 
 void setup()
 {
+  Serial.println("Blimp booting...");
   pinMode(faultPin, INPUT);
   pinMode(igniterPin, OUTPUT);
   Wire.beginOnPins(sclPin, sdaPin);
@@ -38,6 +42,8 @@ void setup()
   delay(20);
   Vset = 0x3F;
   lastPing = millis();
+
+  // Do the motor dance.
   testMotors(0x3F, 200);
   Serial.println("ready to go!"); 
 }
@@ -48,19 +54,22 @@ void loop()
 
   if (digitalRead(faultPin) == LOW) {
     Serial.println(" ----FAULT----  ");
+    getFault(MOTOR1);
+    getFault(MOTOR2);
+    getFault(MOTOR3);
     clearAllFaults();
   }
 
   if ((motorMillis - lastPing) > motorTimeout && timeoutPossible == 1){
-    setBrake(MOTOR2);
-    setBrake(MOTOR3);
+    initDevices();
     timeoutPossible = 0; //can only timeout once
     Serial.println(" TIMED OUT ");
   }
 
   if (millis()-igniterMillis > igniterTimeout){
+    Serial.println("Igniter timed out, turning it off.");
     igniterMillis = millis();
-    setIgniter(0x00);
+    updateIgniter(0x00);
   }
 }
 
@@ -83,7 +92,8 @@ void RFduinoBLE_onDisconnect() {
 
 
 /*
-radio messages should be multiples of 3 bytes: ([channelNum,msg1,msg2] * n)
+radio messages should be two hearder bytes [protoVersion, msgCount]
+followed by multiples of 3 bytes: ([channelNum,msg1,msg2] * n)
 how msg1 ang msg2 bytes will be understood depends on the channel.
 for motor channels (00-02) it will be [motorNum,motorDirectionCode,motorSpeed]
 for igniter channel (03) it will be [channelNum,duration1,duration2]
@@ -99,54 +109,94 @@ void RFduinoBLE_onReceive(char *data, int len) {
   char buf[256];
   sprintf(buf, "message received: [p:%02x] [c:%02x] [l:%d]\n", protoVersion, msgCounter, len);
   Serial.print(buf);
+
   // If there is a protocol version mismatch, ignore all messages.
   if (protoVersion != PROTOCOL_VERSION) {
     Serial.printf("VERSION MISMATCH: Expected %d, received %d.\n", 0, PROTOCOL_VERSION);
     return;
   }
+
+  // Check for any lost messages.
+  if (msgCounter == 0xff) {
+    // 0xff means a new connection, and we start at 0 from there.
+    Serial.println("New connection received.");
+    expectedMsgCounter = 0x00;
+  } else if (expectedMsgCounter == 0xff) {
+    // If we received an ongoing counter but haven't seen a start message, just note it.
+    Serial.printf("Unexpected counter %d with no start message.\n", msgCounter);
+    expectedMsgCounter = (msgCounter + 1) % 255;
+  } else if (msgCounter == expectedMsgCounter) {
+    // All is proceeding as normal.
+    expectedMsgCounter = (msgCounter + 1) % 255;
+  } else {
+    // We lost packets.
+    char lostMessages = msgCounter - expectedMsgCounter;
+    if (lostMessages < 0)
+      lostMessages += 255;      
+    Serial.printf("LOST %d MESSAGES. (Expected counter %d, received %d.)\n", 
+      lostMessages, expectedMsgCounter, msgCounter);
+    expectedMsgCounter = (msgCounter + 1) % 255;
+  }
   
-  // We are assuming that any full block of three bytes can be interpreted, and any remainder 
-  for (int cmdStart=2; cmdStart+3 <= len; cmdStart+=3)
+  // We are assuming that any full block of three bytes can be interpreted, 
+  // and any remainder is ignored.
+  for (int cmdStart=0; cmdStart+3 <= len; cmdStart+=3)
   {
-    if (0x00<=data[cmdStart+0] && data[cmdStart+0]<=0x02){
-      receiveMotorCommand(motors[data[cmdStart+0]], data[cmdStart+1], data[cmdStart+2]);
+    // If it's a motor command, parse it.  
+    // If it's an igniter command and the state has changed, update the igniter.
+    // State changes are identified in updateIgniter() and receiveMotorCommand().
+    if (0x00<=data[cmdStart+0] && data[cmdStart+0]<=0x02) {
+      receiveMotorCommand(data[cmdStart+0], motorIndexes[data[cmdStart+0]], data[cmdStart+1], data[cmdStart+2]);
     }
     else if (data[cmdStart+0] == 0x03) {
-      setIgniter(data[cmdStart+1]);
+      updateIgniter(data[cmdStart+1]);
     }
   }
-  // now that we've recieved data it's possible to timeout.
+  // now that we've recieved valid data it's possible to timeout.
   timeoutPossible = 1;
 }
 
 
-void receiveMotorCommand(byte motor, uint8_t value, uint8_t speed) {
+void receiveMotorCommand(byte motorNum, byte motorIndex, uint8_t value, uint8_t speed) {
+  
+  // If the value has not changed, simply return.
+  if (motorStates[motorNum][0] == value &&
+      motorStates[motorNum][1] == speed)
+      return;
+      
+  // Otherwise, update the motor setting and store the new values.
   if (value == 0x01) {
-    setForward(motor, speed);
+    setForward(motorIndex, speed);
   } else if (value == 0x02) {
-    setReverse(motor, speed);
+    setReverse(motorIndex, speed);
   } else {
-    setBrake(motor);
+    setBrake(motorIndex);
   }
+  motorStates[motorNum][0] = value;
+  motorStates[motorNum][1] = speed;
 }
 
-void setIgniter(byte igniterCode){
+void setIgniter(byte igniterCode) {
   if (igniterCode==0x01){
     digitalWrite(igniterPin,HIGH);
     igniterMillis=millis();
     } else {
       digitalWrite(igniterPin,LOW);
     }
+  igniterState = igniterCode;
+  Serial.printf("set igniter %d\n", igniterCode);
 }
 
-
-void sendMessage(byte motor, uint8_t value, String msg) {
+void updateIgniter(byte igniterCode) {
+  if (igniterCode != igniterState)
+    setIgniter(igniterState);
+}
+void sendMessage(byte motor, uint8_t value, char *msg) {
   Wire.beginTransmission(motor);
   Wire.write(CONTROL);
   Wire.write(value);
   wireAck = Wire.endTransmission();
-  Serial.print(msg+" ");
-  Serial.println();
+  Serial.printf("set motor %d: %s\n", motor, msg);
 }
 
 void setCoast(byte thisMotor){
@@ -169,6 +219,40 @@ void setBrake(byte thisMotor){
   sendMessage(thisMotor, value, "brake"); 
 }
 
+void getFault(int thisMotor){
+  uint8_t RegisterFault;
+
+  Wire.beginTransmission(thisMotor);
+  Wire.write(FAULT);
+  Serial.printf("Result of fault request for motor %d: ", thisMotor);
+  Serial.println(Wire.endTransmission());
+
+  Wire.requestFrom(thisMotor,1);
+  while(Wire.available()){
+    RegisterFault = Wire.read();
+    Serial.print("RegisterFault: ");
+    Serial.print(RegisterFault, HEX);
+    Serial.print(" (");
+
+    if(RegisterFault & 0x01) //fault bit
+      Serial.print(" FAULT ");
+
+    if(RegisterFault & 0x02) //OCP event
+      Serial.print(" OCP ");
+
+    if(RegisterFault & 0x04) //UVLO event
+      Serial.print(" UVLO ");
+
+    if(RegisterFault & 0x08) //OTS event
+      Serial.print(" OTS ");
+
+    if(RegisterFault & 0x10) //ILIMIT event
+      Serial.print(" ILIMIT ");
+
+    Serial.println(")");
+  }
+}
+
 void clearFault(byte thisMotor){
   Wire.beginTransmission(thisMotor);
   Wire.write(FAULT);
@@ -183,8 +267,18 @@ void clearAllFaults() {
   clearFault(MOTOR3);
 }
 
+void initDevices(void) {
+  // Make sure motors and igniter are off.
+  setIgniter(0);
+  setBrake(MOTOR1);
+  setBrake(MOTOR2);
+  setBrake(MOTOR3);
+}
+
 void testMotors(uint8_t velocity, int interval)
 {
+  initDevices();
+  
   delay(25);
   clearAllFaults();
   delay(25);
