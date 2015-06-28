286a8152b35224bf48b81ba59bb1b9d4e0232106
Jef Pearlman <github@jef-pearlman.net>
2015-06-28 11:07:49 -0700

    Change dbus to session so it is possible to control state from other programs.

--- tools/xboxdrv.sh
+++ tools/xboxdrv.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo xboxdrv -d --dbus disabled --daemon --led 2 --mimic-xpad --next-controller --led 3 --mimic-xpad --next-controller --led 4 --mimic-xpad --next-controller --led 5 --mimic-xpad
+sudo xboxdrv -d --dbus session --daemon --led 2 --mimic-xpad --next-controller --led 3 --mimic-xpad --next-controller --led 4 --mimic-xpad --next-controller --led 5 --mimic-xpad
