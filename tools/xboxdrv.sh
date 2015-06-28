b5b99f86787424d3810c4a3fdae87b90746a9f5a
Jef Pearlman <github@jef-pearlman.net>
2015-06-28 11:03:23 -0700

    Make controllers mimic xpad layout so we don't need to remap everything.

--- tools/xboxdrv.sh
+++ tools/xboxdrv.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo xboxdrv -d --dbus disabled --daemon --led 2 --next-controller --led 3 --next-controller --led 4 --next-controller --led 5
+sudo xboxdrv -d --dbus disabled --daemon --led 2 --mimic-xpad --next-controller --led 3 --mimic-xpad --next-controller --led 4 --mimic-xpad --next-controller --led 5 --mimic-xpad
