0083290b94b6506af87111027a05ba7653dff839
Jef Pearlman <github@jef-pearlman.net>
2015-08-24 14:59:45 -0700

    Fix missing controller (resulting in incorrect LED on one controller, as well).

--- tools/xboxdrv.sh
+++ tools/xboxdrv.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo xboxdrv -v -d --dbus session --daemon --led 2 --mimic-xpad --next-controller --led 3 --mimic-xpad --next-controller --led 4 --mimic-xpad --next-controller --led 5 --mimic-xpad --led 2 --mimic-xpad --next-controller --led 3 --mimic-xpad --next-controller --led 4 --mimic-xpad --next-controller --led 5 --mimic-xpad
+sudo xboxdrv -v -d --dbus session --daemon --led 2 --mimic-xpad --next-controller --led 3 --mimic-xpad --next-controller --led 4 --mimic-xpad --next-controller --led 5 --mimic-xpad --next-controller --led 2 --mimic-xpad --next-controller --led 3 --mimic-xpad --next-controller --led 4 --mimic-xpad --next-controller --led 5 --mimic-xpad
